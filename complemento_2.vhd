7319188d30da9e9a90989c2e1a38900aced7debb
Roberto-Ceschini <robert17ceschini@gmail.com>
2024-09-13 00:10:09 -0300

    Create: complemento_2 e porta_not
    Apenas criei os arquivos como teste, não implementei nenhum código.

---
+++ complemento_2.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity complemento_2 is 
+
+    port (
+
+        num: in std_logic_vector (3 downto 0); --Entradas: numero a ser complementado
+        saida: out std_logic_vector (3 downto 0) -- Saidas: numero complementado
+    );
+
+end complemento_2;
+
+architecture comportamento of complemento_2 is -- Funcao: realiza o complemento a 2 de um numero
+
+    begin
+     --falta fazer a entidade
+    end comportamento;
