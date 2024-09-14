fe087f9e2731997b8935ecf64a8592c0cfd2731c
Roberto-Ceschini <robert17ceschini@gmail.com>
2024-09-14 18:50:52 -0300

    Create ULA.vhd
    Precisa fazer o codigo

---
+++ ULA.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity ULA is
+
+    port(
+
+    );
+    
+    end ULA;
+
+architecture comportamento of ULA is 
+
+    begin
+        --Fazer a ULA
+    
+    end comportamento;
