b97270f6d7aed55fb6e7556dc262fed68345eaa3
LucasFL15 <lucasfreitasl15122003@gmail.com>
2024-11-28 00:18:49 -0300

    TOMA

--- TRAB2/Controlador.vhd
+++ TRAB2/Controlador.vhd
@@ -15,6 +15,58 @@ end controlador;
 
 architecture comportamento of controlador is
 
-    begin
 
+
+type estado_type is (receber_A, receber_B, mostrar_res);
+signal estado, proximo_estado : estado_type;
+
+begin
+
+numeros : entity work.Numeros_teclado(comportamento)
+    port map (codigo_teclado => codigo_teclado, a0 => numeroIntermediario(3), a1 => numeroIntermediario(2), a2 => numerointermediario(1), a3 => numerointermediario(0));
+
+seletor : entity work.SeletorOperacao(Behavioral)
+    port map (A3 => a3, A2 => a2, A1 => a1, A0 => a0, 
+                B3 => b3, B2 => b2, B1 => b1, B0 => b0,
+                seletor => operacao,
+                Z7 => z7, Z6 => z6, Z5 => z5, Z4 => z4, Z3 => z3, Z2 => z2, Z1 => z1, Z0 => z0);
+
+
+    process(codigo_teclado)
+        begin
+            if codigo_teclado = "11111111" then
+                estado <= proximo_estado;
+            end if;
+        end process;
+        
+    process(estado)
+        begin
+            case estado is
+                when receber_A =>
+                    a3 <= numeroIntermediario(0)
+                    a2 <= numeroIntermediario(1)
+                    a1 <= numeroIntermediario(2)
+                    a0 <= numeroIntermediario(3)
+                    proximo_estado <= receber_B;
+                
+                when receber_B =>
+                    b3 <= numeroIntermediario(0)
+                    b2 <= numeroIntermediario(1)
+                    b1 <= numeroIntermediario(2)
+                    b0 <= numeroIntermediario(3)
+                    proximo_estado <= mostrar_res
+        
+                when mostrar_res =>
+                    z0 <= z0
+                    z1 <= z1
+                    z2 <= z2
+                    z3 <= z3
+                    z4 <= z4
+                    z5 <= z5
+                    z6 <= z6
+                    z7 <= z7
+            end case;
+    end process;
+
+                    
 end comportamento;
